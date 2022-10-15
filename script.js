5d15ec16f17800bc93b81da948f27aa4335fffc6
MiftikCZ <89579269+MiftikCZ@users.noreply.github.com>
2022-10-15 13:32:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -24,11 +24,11 @@ function keyPressed(key) {
 }
 
 function setup() {
-    createCanvas(1200, 600,"",`
+    createCanvas(1200, 600,"border-bottom:#f003 3px solid;margin-bottom:20px",`
     
     
-    <input type="number" id="size" placeholder="'15' napis size">
-    <button onclick="zmenSize()">nastavit</button>
+    <input type="text" id="size" placeholder="'15' napiš délku" class="inp1">
+    <button onclick="zmenSize()" class="inp2">nastavit</button>
     `)
     for(let i=0;i<size;i++) {
         tail.push([0,0])
@@ -39,8 +39,9 @@ function setup() {
 function changedScene() { }
 
 function update(game, delta) {
+    document.getElementById("_mmade_canv").width=window.innerWidth
     deltaAll+=delta
-    if(qclicked) setBackground("#000")
+    if(qclicked) setBackground("#080808")
     render(tail, `hsl(${deltaAll*60},37%,48%)`, eclicked, 5,wclicked)
     tail.shift()
     tail.push(mousePos)
@@ -48,7 +49,6 @@ function update(game, delta) {
 
 function zmenSize() {
     let velikost = document.getElementById("size").value
-    document.querySelector("body").style.background = `hsl(40,${20*(Math.cos(deltaAll)*57)+50}%,${velikost*2}%)`
     size = velikost
     tail = []
     for(let i=0;i<size;i++) {
