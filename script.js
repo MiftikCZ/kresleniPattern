11070584c1c8e7e85b783a945e9dbd01d3d2e089
MiftikCZ <89579269+MiftikCZ@users.noreply.github.com>
2022-10-15 13:16:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ let tail = [
 let eclicked = false
 let qclicked = true
 let xclicked = false
+let wclicked = true
 let mousePos = [0,0]
 let size = 15
 let deltaAll = 0
@@ -19,6 +20,7 @@ function keyPressed(key) {
     if(key=="e")eclicked=!eclicked
     if(key=="q")qclicked=!qclicked
     if(key=="x")xclicked=!xclicked
+    if(key=="w")wclicked=!wclicked
 }
 
 function setup() {
@@ -28,9 +30,6 @@ function setup() {
     <input type="number" id="size" placeholder="'15' napis size">
     <button onclick="zmenSize()">nastavit</button>
     `)
-    objects.new("trojuhelnik1", [
-        []
-    ], "hsl(40,40%,48%)", false, 3)
     for(let i=0;i<size;i++) {
         tail.push([0,0])
     }
@@ -41,8 +40,8 @@ function changedScene() { }
 
 function update(game, delta) {
     deltaAll+=delta
-    if(qclicked) setBackground("#000")  
-    render(tail, `hsl(${deltaAll*60},37%,48%)`, eclicked, 5)
+    if(qclicked) setBackground("#000")
+    render(tail, `hsl(${deltaAll*60},37%,48%)`, eclicked, 5,wclicked)
     tail.shift()
     tail.push(mousePos)
 }
